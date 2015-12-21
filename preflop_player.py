ed20f71437c3e22db1d0170db3b7afd751d869f2
Eric Ho <eric.ho@yale.edu>
2015-12-21 04:24:44 -0500

    finished players

--- preflop_player.py
+++ preflop_player.py
@@ -106,7 +106,6 @@ def play_afterflop(hand, table, money_in, money_required, big_blind, stack_size,
   kRandomFactor = .1 # max value of random factor
   kReallyRandomFactor = 20
   kSmallBetFactor = .5
-  kStretchFactor = 1.2
   kLimp = .2 # chance that hero limps in with a poor hand
   final_bet = 0
 
@@ -164,6 +163,163 @@ def play_afterflop(hand, table, money_in, money_required, big_blind, stack_size,
             final_bet = int(tentative_bet)
     else: 
       # kStretchFactor to err on the side of calling a bit
+      if cost_benefit_ratio < randomPermute(win_ratio, kRandomFactor):
+        final_bet = money_required - money_in
+      else:
+        final_bet = -1
+
+  if final_bet > (stack_size - money_in):
+    return stack_size - money_in
+  else:
+    return final_bet
+
+# as play goes on, the opponent will probably have a better hand since they're still playing
+# we play more cautiously as a result
+def play_turn(hand, table, money_in, money_required, big_blind, stack_size, position = False, first_bet = True):
+  kUndercut = .4 # chance that hero bets small with a really good hand
+  kReallyGoodRatio = .75
+  kBetRatio = .65 # win percentage that needs to be exceeded to bet first
+  kGoodRatio = .55
+  kBetFactor = 2
+  kConstantBetFactor = .75
+  kRandomFactor = .1 # max value of random factor
+  kReallyRandomFactor = 20
+  kSmallBetFactor = .5
+  kRiskFactor = .05
+  kStretchFactor = .9
+  kLimp = .2 # chance that hero limps in with a poor hand
+  final_bet = 0
+
+  hand_strength = afterturn_sim.getStrength(hand, table)
+  win_ratio = hand_strength[0] / (hand_strength[0] + hand_strength[2])
+  cost_benefit_ratio = (money_required - money_in) / float(money_required)
+
+  # correcting for advantageous position
+  if position:
+    win_ratio = win_ratio * (1 + kPositionAdvantage)
+  else:
+    win_ratio = win_ratio * (1 - kPositionDisadvantage)
+
+  if first_bet:
+    if win_ratio > kBetRatio:
+      if random.random() < kUndercut:
+        if random.random() < kUndercut:
+          final_bet = 0
+        else:
+          final_bet = int(randomPermute(money_in * kSmallBetFactor, kRandomFactor * kReallyRandomFactor))
+      else:
+        final_bet = int(win_ratio * kBetFactor * money_in)
+    else:
+      win_ratio = randomPermute(win_ratio, kRandomFactor)
+      if randomPermute(win_ratio, kRandomFactor) > kGoodRatio or random.random() < kLimp:
+        if random.random() < kLimp:
+          final_bet = int(randomPermute(money_in * kSmallBetFactor, kRandomFactor * kReallyRandomFactor)) # weird bluff bet
+        else:
+          if money_in > big_blind * 2:
+            final_bet = int(randomPermute(big_blind, kRandomFactor))
+          else:
+            final_bet = int(randomPermute(money_in * kSmallBetFactor, kRandomFactor))
+      else:
+        final_bet = 0
+    if final_bet < 0:
+      final_bet = 0
+  else:
+    if win_ratio > kBetRatio:
+      if random.random() < kUndercut:
+        final_bet = money_required - money_in
+      else:
+        if randomPermute(win_ratio, kRandomFactor * 2) < cost_benefit_ratio:
+          if win_ratio > kReallyGoodRatio:
+            final_bet = randomPermute((money_required - money_in) * 2, kRandomFactor)
+          else:
+            final_bet = -1
+        else:
+          tentative_bet = (win_ratio + kConstantBetFactor) * big_blind + win_ratio * (money_required - money_in) * kBetFactor
+          if tentative_bet < money_required - money_in:
+            if win_ratio > kReallyGoodRatio:
+              final_bet = (money_required - money_in) * 2
+            else:
+              final_bet = money_required - money_in
+          else:
+            final_bet = int(tentative_bet)
+    else: 
+      if cost_benefit_ratio < randomPermute(win_ratio, kRandomFactor) * kStretchFactor - kRiskFactor:
+        final_bet = money_required - money_in
+      else:
+        final_bet = -1
+
+  if final_bet > (stack_size - money_in):
+    return stack_size - money_in
+  else:
+    return final_bet
+
+def play_river(hand, table, money_in, money_required, big_blind, stack_size, position = False, first_bet = True):
+  kUndercut = .4 # chance that hero bets small with a really good hand
+  kReallyGoodRatio = .775
+  kBetRatio = .675 # win percentage that needs to be exceeded to bet first
+  kGoodRatio = .6
+  kBetFactor = 2
+  kConstantBetFactor = .75
+  kRandomFactor = .1 # max value of random factor
+  kReallyRandomFactor = 20
+  kSmallBetFactor = .5
+  kStretchFactor = .85
+  kLimp = .2 # chance that hero limps in with a poor hand
+  final_bet = 0
+
+  hand_strength = afterriver_sim.getStrength(hand, table)
+  win_ratio = hand_strength[0] / (hand_strength[0] + hand_strength[2])
+  cost_benefit_ratio = (money_required - money_in) / float(money_required)
+
+  # correcting for advantageous position
+  if position:
+    win_ratio = win_ratio * (1 + kPositionAdvantage)
+  else:
+    win_ratio = win_ratio * (1 - kPositionDisadvantage)
+
+  if first_bet:
+    if win_ratio > kBetRatio:
+      if random.random() < kUndercut:
+        if random.random() < kUndercut:
+          final_bet = 0
+        else:
+          final_bet = int(randomPermute(money_in * kSmallBetFactor, kRandomFactor * kReallyRandomFactor))
+      else:
+        final_bet = int(win_ratio * kBetFactor * money_in)
+    else:
+      win_ratio = randomPermute(win_ratio, kRandomFactor)
+      if randomPermute(win_ratio, kRandomFactor) > kGoodRatio or random.random() < kLimp:
+        if random.random() < kLimp:
+          final_bet = int(randomPermute(money_in * kSmallBetFactor, kRandomFactor * kReallyRandomFactor)) # weird bluff bet
+        else:
+          if money_in > big_blind * 2:
+            final_bet = int(randomPermute(big_blind, kRandomFactor))
+          else:
+            final_bet = int(randomPermute(money_in * kSmallBetFactor, kRandomFactor))
+      else:
+        final_bet = 0
+    if final_bet < 0:
+      final_bet = 0
+  else:
+    if win_ratio > kBetRatio:
+      if random.random() < kUndercut:
+        final_bet = money_required - money_in
+      else:
+        if randomPermute(win_ratio, kRandomFactor * 2) < cost_benefit_ratio:
+          if win_ratio > kReallyGoodRatio:
+            final_bet = randomPermute((money_required - money_in) * 2, kRandomFactor)
+          else:
+            final_bet = -1
+        else:
+          tentative_bet = (win_ratio + kConstantBetFactor) * big_blind + win_ratio * (money_required - money_in) * kBetFactor
+          if tentative_bet < money_required - money_in:
+            if win_ratio > kReallyGoodRatio:
+              final_bet = (money_required - money_in) * 2
+            else:
+              final_bet = money_required - money_in
+          else:
+            final_bet = int(tentative_bet)
+    else: 
       if cost_benefit_ratio < randomPermute(win_ratio, kRandomFactor) * kStretchFactor:
         final_bet = money_required - money_in
       else:
@@ -174,34 +330,19 @@ def play_afterflop(hand, table, money_in, money_required, big_blind, stack_size,
   else:
     return final_bet
 
-def play_turn(hand, table, money_in, money_required, big_blind, stack_size, position = False, first_bet = True):
-  hand_strength = afterturn_sim.getStrength(hand, table)
-  win_ratio = hand_strength[0] / (hand_strength[0] + hand_strength[2])
-  print win_ratio
-
-  return money_required - money_in
-
-def play_river(hand, table, money_in, money_required, big_blind, stack_size, position = False, first_bet = True):
-  hand_strength = afterriver_sim.getStrength(hand, table)
-  win_ratio = hand_strength[0] / (hand_strength[0] + hand_strength[2])
-  print win_ratio
-
-  return money_required - money_in
-
-
 #central tenant #1 - aggressive AI is better than passive AI
 #central tenant #2 - AI is good because of randomness
 #central tenant #3 - AI is good because of monte carlo
 
-theDeck = cards.Deck()
-theDeck.shuffle()
-hand = cards.Hand()
-table = cards.Hand()
-hand.add_card(theDeck.deal_card())
-hand.add_card(theDeck.deal_card())
-
-for j in range(3):
-  table.add_card(theDeck.deal_card())
-
-print hand, table
-print play_afterflop(hand, table, 10, 20, 10, 300, True, False)
+#theDeck = cards.Deck()
+#theDeck.shuffle()
+#hand = cards.Hand()
+#table = cards.Hand()
+#hand.add_card(theDeck.deal_card())
+#hand.add_card(theDeck.deal_card())
+#
+#for j in range(5):
+#  table.add_card(theDeck.deal_card())
+#
+#print hand, table
+#print play_river(hand, table, 30, 50, 10, 300, True, False)
